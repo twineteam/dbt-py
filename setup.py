bae1aa3122f6bc6cc861d4567b31253815292f47
Jon Zanillo <50558673+jzanillo@users.noreply.github.com>
2022-11-07 12:14:29 -0500

    [UPD] dbt-core -> use wildcard v1.+

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ except (IOError, ImportError):
     long_description = "Python dbt executor with custom logging and alerting that fits into your stack"
 
 INSTALL_REQUIRES = [
-    'dbt-core==1.0.*', 'datadog==0.40.1', 'python-dotenv==0.17.0',
+    'dbt-core==1.*', 'datadog==0.40.1', 'python-dotenv==0.17.0',
     'pygments>=2.4.0', 'sentry-sdk==1.4.*', "prometheus-client==0.11.0"
 ]
 
